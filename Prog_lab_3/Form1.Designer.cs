b74182f666195d3180c724bf7b4222df202aa506
Руслан Мирзаев <ruslanmirzaev16621@gmail.com>
2021-10-10 18:24:19 +0700

    Ready

---
+++ Prog_lab_3/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace Prog_lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.calculateBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextMinPointBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.backMinPointBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearFieldBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitBtn = new System.Windows.Forms.ToolStripMenuItem();
+            this.bottomBorderField = new System.Windows.Forms.TextBox();
+            this.topBorderField = new System.Windows.Forms.TextBox();
+            this.accuracyField = new System.Windows.Forms.TextBox();
+            this.formulaField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.answerField = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculateBtn,
+            this.nextMinPointBtn,
+            this.backMinPointBtn,
+            this.clearFieldBtn,
+            this.exitBtn});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(746, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(98, 24);
+            this.calculateBtn.Text = "Рассчитать";
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // nextMinPointBtn
+            // 
+            this.nextMinPointBtn.Enabled = false;
+            this.nextMinPointBtn.Name = "nextMinPointBtn";
+            this.nextMinPointBtn.Size = new System.Drawing.Size(74, 24);
+            this.nextMinPointBtn.Text = "Вперед";
+            this.nextMinPointBtn.Click += new System.EventHandler(this.nextMinPointBtn_Click);
+            // 
+            // backMinPointBtn
+            // 
+            this.backMinPointBtn.Enabled = false;
+            this.backMinPointBtn.Name = "backMinPointBtn";
+            this.backMinPointBtn.Size = new System.Drawing.Size(65, 24);
+            this.backMinPointBtn.Text = "Назад";
+            this.backMinPointBtn.Click += new System.EventHandler(this.backMinPointBtn_Click);
+            // 
+            // clearFieldBtn
+            // 
+            this.clearFieldBtn.Enabled = false;
+            this.clearFieldBtn.Name = "clearFieldBtn";
+            this.clearFieldBtn.Size = new System.Drawing.Size(87, 24);
+            this.clearFieldBtn.Text = "Очистить";
+            this.clearFieldBtn.Click += new System.EventHandler(this.clearFieldBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(67, 24);
+            this.exitBtn.Text = "Выход";
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // bottomBorderField
+            // 
+            this.bottomBorderField.Location = new System.Drawing.Point(52, 66);
+            this.bottomBorderField.Name = "bottomBorderField";
+            this.bottomBorderField.Size = new System.Drawing.Size(100, 22);
+            this.bottomBorderField.TabIndex = 1;
+            // 
+            // topBorderField
+            // 
+            this.topBorderField.Location = new System.Drawing.Point(219, 66);
+            this.topBorderField.Name = "topBorderField";
+            this.topBorderField.Size = new System.Drawing.Size(100, 22);
+            this.topBorderField.TabIndex = 2;
+            // 
+            // accuracyField
+            // 
+            this.accuracyField.Location = new System.Drawing.Point(381, 66);
+            this.accuracyField.Name = "accuracyField";
+            this.accuracyField.Size = new System.Drawing.Size(100, 22);
+            this.accuracyField.TabIndex = 3;
+            // 
+            // formulaField
+            // 
+            this.formulaField.Location = new System.Drawing.Point(534, 66);
+            this.formulaField.Name = "formulaField";
+            this.formulaField.Size = new System.Drawing.Size(144, 22);
+            this.formulaField.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(381, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "e";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(534, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "f(x)";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(31, 175);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(685, 463);
+            this.zedGraphControl1.TabIndex = 9;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // answerField
+            // 
+            this.answerField.Location = new System.Drawing.Point(52, 131);
+            this.answerField.Name = "answerField";
+            this.answerField.Size = new System.Drawing.Size(192, 22);
+            this.answerField.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Точка минимума";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 677);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.answerField);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.formulaField);
+            this.Controls.Add(this.accuracyField);
+            this.Controls.Add(this.topBorderField);
+            this.Controls.Add(this.bottomBorderField);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem calculateBtn;
+        private System.Windows.Forms.ToolStripMenuItem nextMinPointBtn;
+        private System.Windows.Forms.ToolStripMenuItem backMinPointBtn;
+        private System.Windows.Forms.ToolStripMenuItem clearFieldBtn;
+        private System.Windows.Forms.ToolStripMenuItem exitBtn;
+        private System.Windows.Forms.TextBox bottomBorderField;
+        private System.Windows.Forms.TextBox topBorderField;
+        private System.Windows.Forms.TextBox accuracyField;
+        private System.Windows.Forms.TextBox formulaField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.TextBox answerField;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Prog_lab_3/Form1.cs
@@ -0,0 +1,262 @@
+﻿using MathNet.Symbolics;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Prog_lab_3
+{
+    public partial class Form1 : Form
+    {
+        List<Point> minPointList = new List<Point>();
+        PointPairList list = new PointPairList();
+        double bottomBorder, topBorder, accuracy;
+        int count;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private int getNumberDecimalPlaces()
+        {
+            int decimalPlaces = 0;
+
+            for(double i = accuracy; i < 1; i *= 10)
+            {
+                ++decimalPlaces;
+            }
+            return decimalPlaces;
+        }
+
+        private double calculateValuePoint(double value)
+        {
+            double result = 0;
+            var variables = new Dictionary<string, FloatingPoint> { { "x", value } };
+            var formula = Infix.ParseOrThrow(formulaField.Text);
+            result = Evaluate.Evaluate(variables, formula).RealValue;
+            return result;
+        }
+
+        double findMin()
+        {
+            double PHI = (1 + Math.Sqrt(5)) / 2;
+
+            double x1, x2, a = bottomBorder, b = topBorder;
+            while (true)
+            {
+                x1 = b - (b - a) / PHI;
+                x2 = a + (b - a) / PHI;
+                if (calculateValuePoint(x1) >= calculateValuePoint(x2))
+                {
+                    a = x1;
+                    minPointList.Add(new Point(x1, calculateValuePoint(x1)));
+                }
+                else
+                {
+                    b = x2;
+                    minPointList.Add(new Point(x2, calculateValuePoint(x2)));
+                }
+
+                if (Math.Abs(b - a) < accuracy)
+                {
+                    break;
+                }
+            }
+            minPointList.Add(new Point((a + b) / 2, calculateValuePoint((a + b) / 2)));
+            return (a + b) / 2;
+        }
+
+        private void drawGraph(double xPoint, double yPoint)
+        {
+            GraphPane mypane = zedGraphControl1.GraphPane;
+            mypane.CurveList.Clear();
+
+            mypane.XAxis.MajorGrid.IsVisible = true;
+            mypane.XAxis.MinorGrid.IsVisible = true;
+            mypane.YAxis.MajorGrid.IsVisible = true;
+
+            LineItem myCurve = mypane.AddCurve("f(x)", list, Color.Blue, SymbolType.None);
+            LineItem minPointCurve = mypane.AddCurve("Min point", new double[] { xPoint }, new double[] { yPoint }, Color.DarkRed, SymbolType.Circle);
+            minPointCurve.Symbol.Fill.Type = FillType.Solid;
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                Environment.Exit(0);
+            }).Start();
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            new Thread(() =>
+            {
+                try
+                {
+                    Invoke((Action)(() =>
+                    {
+                        backMinPointBtn.Enabled = false;
+                        nextMinPointBtn.Enabled = false;
+                        clearFieldBtn.Enabled = false;
+                        GraphPane mypane = zedGraphControl1.GraphPane;
+                        mypane.CurveList.Clear();
+                        zedGraphControl1.AxisChange();
+                        zedGraphControl1.Invalidate();
+                        answerField.Text = "";
+                    }));
+
+
+                    minPointList.Clear();
+                    if (double.TryParse(bottomBorderField.Text, out bottomBorder) && double.TryParse(topBorderField.Text, out topBorder)
+                        && double.TryParse(accuracyField.Text, out accuracy) && formulaField.Text.Length > 0)
+                    {
+                        if (bottomBorder >= topBorder)
+                        {
+                            MessageBox.Show("Значение a должно быть меньше значения b");
+                            return;
+                        }
+                        else if (accuracy <= 0)
+                        {
+                            MessageBox.Show("Точность должна быть больше 0");
+                            return;
+                        }else if (topBorder - bottomBorder < accuracy)
+                        {
+                            MessageBox.Show("Границы функции меньше точности, измените входные данные");
+                            return;
+                        }
+
+                        double xMinPoint = findMin();
+                        double yMinPoint = calculateValuePoint(xMinPoint);
+
+                        calculationGraphPoints();
+                        drawGraph(xMinPoint, yMinPoint);
+                        count = minPointList.Count - 1; 
+
+                        Invoke((Action)(() =>
+                        {
+                            answerField.Text = "x: " + Math.Round(xMinPoint, getNumberDecimalPlaces()).ToString() + " y: " + Math.Round(yMinPoint, getNumberDecimalPlaces()).ToString();
+                            backMinPointBtn.Enabled = true;
+                            clearFieldBtn.Enabled = true;
+                        }));
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверные входные данные");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Неверная функция или слишком большие границы");
+                }
+
+            }).Start();
+        }
+
+        private void clearFieldBtn_Click(object sender, EventArgs e)
+        {
+            foreach (Control element in Controls)
+            {
+                if (element is TextBox)
+                {
+                    ((TextBox)element).Text = null;
+                }
+            }
+        }
+
+        private void nextMinPointBtn_Click(object sender, EventArgs e)
+        {
+            if (count < minPointList.Count - 1)
+            {
+                count++;
+                drawGraph(minPointList[count].x, minPointList[count].y);
+                //label1.Text = Math.Round(minPointList[count].x, 3).ToString();
+                //label2.Text = minPointList.Count.ToString();
+                //label3.Text = count.ToString();
+            }
+
+            if (count >= minPointList.Count - 1)
+            {
+                nextMinPointBtn.Enabled = false;
+            }
+
+            if (count > 0)
+            {
+                backMinPointBtn.Enabled = true;
+            }
+        }
+
+        private void backMinPointBtn_Click(object sender, EventArgs e)
+        {
+            if (count > 0)
+            {
+                --count;
+                drawGraph(minPointList[count].x, minPointList[count].y);
+                //label1.Text = Math.Round(minPointList[count].x, 3).ToString();
+                //label2.Text = minPointList.Count.ToString();
+                //label3.Text = count.ToString();
+            }
+
+
+            if (count == 0)
+            {
+                backMinPointBtn.Enabled = false;
+            }
+
+
+            if (count < minPointList.Count - 1)
+            {
+                nextMinPointBtn.Enabled = true;
+            }
+        }
+
+        private void calculationGraphPoints()
+        {
+            list.Clear();
+            for (double x = bottomBorder; x < topBorder; x += 0.01)
+            {
+                try
+                {
+                    list.Add(x, calculateValuePoint(x));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Слишком большая точность или интервал");
+                }
+            }
+        }
+
+    }
+}
+
+
+class Point
+{
+    public double x, y;
+
+
+    public Point()
+    {
+
+    }
+
+    public Point(double xKoordinate, double yKoordinate)
+    {
+        x = xKoordinate;
+        y = yKoordinate;
+    }
+}
